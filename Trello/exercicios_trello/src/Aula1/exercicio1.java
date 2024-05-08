a1bb8327a40da74a4e73f22c13538ab4fb7995d3
axpedro <pedruax@hotmail.com>
2024-05-08 20:17:51 -0300

    Exercício 1  do TRello

--- Trello/exercicios_trello/src/Aula1/exercicio1.java
+++ Trello/exercicios_trello/src/Aula1/exercicio1.java
@@ -1,7 +1,20 @@
 package Aula1;
 
+import java.util.Scanner;
+
 public class exercicio1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+
+        System.out.println("Insira a largura do terreno: ");
+        x = scan.nextInt();
+        System.out.println("Insira o comprimento do terreno: ");
+        y = scan.nextInt();
+        int z = x * y;
+        System.out.println("O terreno possui: " + z +"m2 de area.");
+
 
     }
 }
