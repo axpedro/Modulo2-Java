47bfaedda5703b644dd343dd9bc9c70f4c66be3a
axpedro <pedruax@hotmail.com>
2024-06-07 20:56:26 +0200

    classe consulta e seus atributos

---
+++ Trello/exercicios_trello/src/semana2/Consulta.java
@@ -0,0 +1,17 @@
+package semana2;
+
+import semana2.entidades.Nutricionista;
+import semana2.entidades.Paciente;
+
+import java.time.LocalDate;
+
+public class Consulta {
+
+    Nutricionista nutricionista;
+    Paciente paciente;
+    LocalDate horaEData;
+    boolean consultaRealida;
+
+}
+
+
